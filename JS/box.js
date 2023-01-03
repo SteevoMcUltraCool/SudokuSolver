23b1f7219563bb1f9f638339cc6e4c1ea4f44a06
SteevoMcCool <96311500+SteevoMcCool@users.noreply.github.com>
2023-01-03 18:11:58 -0500

    hi

--- JS/box.js
+++ JS/box.js
@@ -1,5 +1,6 @@
 let mommy = document.getElementById("bigGrid")
 let Boxes = [
+    false,
 [],
 [],
 [],
@@ -21,10 +22,10 @@ class eGrid {
         mommy.appendChild(this.element)
     }
     boxify(){
-        this.Boxes = ["",
-        ["",new eBox(this,1,1),new eBox(this,1,2),new eBox(this,1,3)],
-        ["",new eBox(this,2,1),new eBox(this,2,2),new eBox(this,2,3)],
-        ["",new eBox(this,3,1),new eBox(this,3,2),new eBox(this,3,3)]
+        this.Boxes = [false,
+            new eBox(this,1,1),new eBox(this,1,2),new eBox(this,1,3), 
+            new eBox(this,2,1),new eBox(this,2,2),new eBox(this,2,3),
+            new eBox(this,3,1),new eBox(this,3,2),new eBox(this,3,3)
         ]
     }
 }
@@ -41,48 +42,51 @@ class eBox {
         this.element.style.gridColumn = localColumn
         this.parent = parent
         this.row = (parent.row-1)*3 + localRow
-        this.column = (parent.column-1)*3 + localRow
+        this.column = (parent.column-1)*3 + localColumn
         Boxes[this.column][this.row]= this
         this.element.appendChild(this.input)
         parent.element.appendChild(this.element)   
         this.potetnialValues =  [null,1,2,3,4,5,6,7,8,9]     
     }
     getValue() {
-        return this.input.value
+        return Number(this.input.value)
     }
     setValue(number) {
         this.potetnialValues = [number]
         this.input.value = number
     }
     elimateValueNearby() {
-        let Row = Boxes[this.row]
+        let Row = Boxes[this.column]
         let Column = [
-            Boxes[1][this.column],
-            Boxes[2][this.column],
-            Boxes[3][this.column],
-            Boxes[4][this.column],            
-            Boxes[5][this.column],
-            Boxes[6][this.column],
-            Boxes[7][this.column],
-            Boxes[8][this.column],
-            Boxes[9][this.column]
+            Boxes[1][this.row],
+            Boxes[2][this.row],
+            Boxes[3][this.row],
+            Boxes[4][this.row],            
+            Boxes[5][this.row],
+            Boxes[6][this.row],
+            Boxes[7][this.row],
+            Boxes[8][this.row],
+            Boxes[9][this.row]
         ]
         let Grid = this.parent.Boxes
         Row.forEach(box=> {
-            if (!box.getValue()){
+            if (box && !box.getValue()){
               box.potetnialValues[this.getValue()] = null  
             }
         })
         Column.forEach(box=> {
-            if (!box.getValue()){
+            if (box && !box.getValue()){
               box.potetnialValues[this.getValue()] = null  
             }
         })
         Grid.forEach(box=> {
-            if (!box.getValue()){
+            if (box && !box.getValue()){
               box.potetnialValues[this.getValue()] = null  
             }
         })
+    }
+    getBoxes() {
+        return Boxes
     }
 }
 
--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,8 @@
 import sheet from "../style.css" assert {type:"css"}
-import {Grid} from "./box.js"
+import {Box, Grid} from "./box.js"
+const DOM = {
+    solveButton: document.getElementById("solveBu")
+}
 document.adoptedStyleSheets = [sheet];
 let Grid1 = new Grid(1,1)
 let Grid2 = new Grid(2,1)
@@ -20,3 +23,40 @@ Grid6.boxify()
 Grid7.boxify()
 Grid8.boxify()
 Grid9.boxify()
+
+function LastNumberRemaining(FilledBoxes, UnfilledBoxes){
+    UnfilledBoxes.forEach(box =>{
+        let change = false
+        let pot = box.potetnialValues.filter(value => value)
+        if (pot.length == 1) {
+            console.log(box.potetnialValues, box.column, box.row)
+            box.setValue(pot[0])
+            box.input.readOnly = true
+            box.input.className = "solved"
+            UnfilledBoxes = UnfilledBoxes.filter(item => item != box)
+            FilledBoxes.push(box)
+            change = true
+        }else if (pot.length == 0) {
+            console.error("puzzle unsolvable");
+        }
+        return [FilledBoxes, UnfilledBoxes, change]
+    })
+}
+DOM.solveButton.addEventListener("click",function(){
+    let Boxes = Box.prototype.getBoxes()
+    let FilledBoxes = []
+    let UnfilledBoxes = []
+    Boxes.forEach(row=>row && row.filter(box => box && box.getValue()).forEach(box => FilledBoxes.push(box)))
+    FilledBoxes.forEach(box=>{
+        console.log(box.input.value)
+        box.input.disabled = true
+        box.elimateValueNearby()
+    })
+    Boxes.forEach(row=>row && row.filter(box => box && !box.getValue()).forEach(box => UnfilledBoxes.push(box)))
+    let change = true
+    do {
+        let b = LastNumberRemaining(FilledBoxes, UnfilledBoxes)
+        console.log(b)
+        FilledBoxes, UnfilledBoxes, change = b[0], b[1], b[2]
+    }while(change)
+})
