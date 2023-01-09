8536b9f415d5c94e916aa9b1668913fbba26fdcf
SteevoMcUltraCool <stephen.radice23@sitechhs.com>
2023-01-09 13:46:48 -0500

    Update methods.js

--- JS/methods.js
+++ JS/methods.js
@@ -1,7 +1,7 @@
 import { Grid, Box } from "./box.js";
 import { Move } from "./moves.js";
-let DOMmoves = document.getElementById("moves")
-let PerformedOperations = []
+let DOMmoves = document.getElementById("moves");
+let PerformedOperations = [];
 const methods = {
   LastNumberRemaining: {
     name: "Last Number Remaining",
@@ -23,9 +23,9 @@ const methods = {
           FilledBoxes.push(box);
           box.elimateValueNearby();
           change = true;
-          new Move("Last Number Remaining",[box], DOMmoves)
+          new Move("Last Number Remaining", [box], DOMmoves);
         } else if (pot.length == 0) {
-          console.error(box,"puzzle unsolvable");
+          console.error(box, "puzzle unsolvable");
         }
       });
       return [FilledBoxes, UnfilledBoxes, change];
@@ -58,7 +58,7 @@ const methods = {
             FilledBoxes.push(box);
             change = true;
             box.elimateValueNearby();
-            new Move("Last Cell Remaining In Grid: ",[box], DOMmoves)
+            new Move("Last Cell Remaining In Grid: ", [box], DOMmoves);
           }
         }
       });
@@ -79,10 +79,12 @@ const methods = {
       let Boxes = Box.prototype.getBoxes();
       Boxes.filter((row) => row).forEach((row) => {
         for (let i = 1; i <= 9; i++) {
-          let boxesWith = row.filter((box) => box && box.potetnialValues.includes(i) && !box.getValue());
+          let boxesWith = row.filter(
+            (box) => box && box.potetnialValues.includes(i) && !box.getValue()
+          );
           if (boxesWith.length == 1) {
             let box = boxesWith[0];
-            console.log("LNR Row:",boxesWith[0], i)
+            console.log("LNR Row:", boxesWith[0], i);
             box.setValue(i);
             box.input.readOnly = true;
             box.input.className = "solved";
@@ -92,7 +94,7 @@ const methods = {
             FilledBoxes.push(box);
             change = true;
             box.elimateValueNearby();
-            new Move("Last Cell Remaining In Row: ",[box], DOMmoves)
+            new Move("Last Cell Remaining In Row: ", [box], DOMmoves);
           }
         }
       });
@@ -114,10 +116,12 @@ const methods = {
       let Columns = Grid.prototype.rotate(Boxes);
       Columns.filter((column) => column).forEach((column) => {
         for (let i = 1; i <= 9; i++) {
-          let boxesWith = column.filter((box) => box && box.potetnialValues.includes(i) && !box.getValue());
+          let boxesWith = column.filter(
+            (box) => box && box.potetnialValues.includes(i) && !box.getValue()
+          );
           if (boxesWith.length == 1) {
             let box = boxesWith[0];
-            console.log("LNR Column:", boxesWith[0],i)
+            console.log("LNR Column:", boxesWith[0], i);
             box.setValue(i);
             box.input.readOnly = true;
             box.input.className = "solved";
@@ -127,7 +131,7 @@ const methods = {
             FilledBoxes.push(box);
             change = true;
             box.elimateValueNearby();
-            new Move("Last Cell Remaining In Column: ",[box], DOMmoves)
+            new Move("Last Cell Remaining In Column: ", [box], DOMmoves);
           }
         }
       });
@@ -136,57 +140,131 @@ const methods = {
   },
   DoubleEliminationInGrid: {
     name: "Double Elimination in Grid",
-    getDescription: (n1,n2) =>
+    getDescription: (n1, n2) =>
       `Pay attention to the highlighted boxes. Since no other box in the grid can be ${n1} or ${n2}, we know these boxes must be ${n1} and ${n2}, so we can eliminate these values from surrounding boxes/rows.`,
     difficulty: 3,
-    difficultyDepracatives: ["DoubleEliminationInRow", "DoubleEliminationInColumn"],
+    difficultyDepracatives: ["DoubleEliminationInRow"],
     operate: function (FilledBoxes, UnfilledBoxes, Grids) {
       let change = false;
       let Boxes = Box.prototype.getBoxes();
       let Columns = Grid.prototype.rotate(Boxes);
-      Object.values(Grids).filter((grid) => grid).forEach((grid) => {
-        let valuesRemaining = [null,1,2,3,4,5,6,7,8,9]
-        let localFilledBoxes = grid.Boxes.filter(box=> box && box.getValue())
-        let localunfilledBoxes = grid.Boxes.filter(box=>box && !box.getValue())
-        localFilledBoxes.forEach(box => valuesRemaining[box.getValue()]=null)
-        valuesRemaining = valuesRemaining.filter(value =>value)
-        valuesRemaining.forEach(function(v){
-          let boxesWith = localunfilledBoxes.filter(box =>box.potetnialValues.includes(v))
-          if (boxesWith.length == 2){
-            valuesRemaining.filter(val => val != v).forEach(function(val){
-              boxesWith = boxesWith.filter(box =>box.potetnialValues.includes(val))
-              let boxWithReduced = localunfilledBoxes.filter(box =>box.potetnialValues.includes(val))
-              if (boxesWith.length==2 && boxWithReduced.length == 2){
-                //elimination time
-                if (PerformedOperations.includes("DEIG"+ boxesWith[0].row + boxesWith[0].column+boxesWith[1].row+boxesWith[1].column)){
-                  return [FilledBoxes, UnfilledBoxes, change]
-                }
-                PerformedOperations.push(("DEIG"+ boxesWith[0].row + boxesWith[0].column+boxesWith[1].row+boxesWith[1].column))
-                change = true
-                if (boxesWith[0].row==boxesWith[1].row){
-                  let row = boxesWith[0].row
-                  Boxes[row].filter(box => box && !box.getValue()).forEach(box => {box.potetnialValues[v]=null; box.potetnialValues[val]=null}) 
-                }else if (boxesWith[0].column == boxesWith[0].column){
-                  let column = boxesWith[0].column
-                  Columns[column].filter(box => box && !box.getValue()).forEach(box => {box.potetnialValues[v]=null; box.potetnialValues[val]=null})                 
-                }
-                localunfilledBoxes.filter(box => box && !box.getValue()).forEach(box => {box.potetnialValues[v]=null; box.potetnialValues[val]=null})
-                boxesWith[0].potetnialValues = [null,null,null,null,null,null,null,null,null,null]
-                boxesWith[1].potetnialValues = [null,null,null,null,null,null,null,null,null,null]
-                boxesWith[0].potetnialValues[v] = v
-                boxesWith[1].potetnialValues[v] = v
-                boxesWith[0].potetnialValues[val] = val
-                boxesWith[1].potetnialValues[val] = val
-                console.log(boxesWith[0].potetnialValues.toString(),boxesWith[1].potetnialValues.toString())
-                //elimination time
-                new Move("Double Elimination in Grid: ",[boxesWith[0],boxesWith[1]], DOMmoves)
-              }
-            })
-          }
-        })
-      });
+      Object.values(Grids)
+        .filter((grid) => grid)
+        .forEach((grid) => {
+          let valuesRemaining = [null, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+          let localFilledBoxes = grid.Boxes.filter(
+            (box) => box && box.getValue()
+          );
+          let localunfilledBoxes = grid.Boxes.filter(
+            (box) => box && !box.getValue()
+          );
+          localFilledBoxes.forEach(
+            (box) => (valuesRemaining[box.getValue()] = null)
+          );
+          valuesRemaining = valuesRemaining.filter((value) => value);
+          valuesRemaining.forEach(function (v) {
+            let boxesWith = localunfilledBoxes.filter((box) =>
+              box.potetnialValues.includes(v)
+            );
+            if (boxesWith.length == 2) {
+              valuesRemaining
+                .filter((val) => val != v)
+                .forEach(function (val) {
+                  boxesWith = boxesWith.filter((box) =>
+                    box.potetnialValues.includes(val)
+                  );
+                  let boxWithReduced = localunfilledBoxes.filter((box) =>
+                    box.potetnialValues.includes(val)
+                  );
+                  if (boxesWith.length == 2 && boxWithReduced.length == 2) {
+                    //elimination time
+                    if (
+                      PerformedOperations.includes(
+                        "DEIG" +
+                          boxesWith[0].row +
+                          boxesWith[0].column +
+                          boxesWith[1].row +
+                          boxesWith[1].column
+                      )
+                    ) {
+                      return [FilledBoxes, UnfilledBoxes, change];
+                    }
+                    PerformedOperations.push(
+                      "DEIG" +
+                        boxesWith[0].row +
+                        boxesWith[0].column +
+                        boxesWith[1].row +
+                        boxesWith[1].column
+                    );
+                    change = true;
+                    if (boxesWith[0].row == boxesWith[1].row) {
+                      let row = boxesWith[0].row;
+                      Boxes[row]
+                        .filter((box) => box && !box.getValue())
+                        .forEach((box) => {
+                          box.potetnialValues[v] = null;
+                          box.potetnialValues[val] = null;
+                        });
+                    } else if (boxesWith[0].column == boxesWith[1].column) {
+                      let column = boxesWith[0].column;
+                      Columns[column]
+                        .filter((box) => box && !box.getValue())
+                        .forEach((box) => {
+                          box.potetnialValues[v] = null;
+                          box.potetnialValues[val] = null;
+                        });
+                    }
+                    localunfilledBoxes
+                      .filter((box) => box && !box.getValue())
+                      .forEach((box) => {
+                        box.potetnialValues[v] = null;
+                        box.potetnialValues[val] = null;
+                      });
+                    boxesWith[0].potetnialValues = [
+                      null,
+                      null,
+                      null,
+                      null,
+                      null,
+                      null,
+                      null,
+                      null,
+                      null,
+                      null,
+                    ];
+                    boxesWith[1].potetnialValues = [
+                      null,
+                      null,
+                      null,
+                      null,
+                      null,
+                      null,
+                      null,
+                      null,
+                      null,
+                      null,
+                    ];
+                    boxesWith[0].potetnialValues[v] = v;
+                    boxesWith[1].potetnialValues[v] = v;
+                    boxesWith[0].potetnialValues[val] = val;
+                    boxesWith[1].potetnialValues[val] = val;
+                    console.log(
+                      boxesWith[0].potetnialValues.toString(),
+                      boxesWith[1].potetnialValues.toString()
+                    );
+                    //elimination time
+                    new Move(
+                      "Double Elimination in Grid: ",
+                      [boxesWith[0], boxesWith[1]],
+                      DOMmoves
+                    );
+                  }
+                });
+            }
+          });
+        });
       return [FilledBoxes, UnfilledBoxes, change];
-    },   
-  }
+    },
+  },
 };
 export let Methods = methods;
